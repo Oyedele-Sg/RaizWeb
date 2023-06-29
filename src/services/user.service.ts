c40b1aa7bf29ab43cbded5110fe0ffe99053fb51
Israel Adefidipe <israeladefidipe@gmail.com>
2023-06-29 10:47:07 +0100

    🖥️ wip: protected routes and pages

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,8 +1,7 @@
 import { BehaviorSubject } from "rxjs"
-
-import Router from "next/router"
 import { fetchWrapper } from "@/utils/fetchWrapper"
 import { URL } from "@/utils/constants"
+import { redirect } from "next/navigation"
 
 const baseUrl = `${URL}`
 const storedUser =
@@ -17,40 +16,40 @@ export const userService = {
     return userSubject.value
   },
 
-  logout,
-  getUser,
+  // logout,
+  // getUser,
 }
 
 // auth
-function login(data): Promise<void> {
-  return fetchWrapper
-    .post(`${baseUrl}/auth/login/`, data)
-    .then((user: typeof fetchWrapper.userValue) => {
-      // publish user to subscribers and store in local storage to stay logged in between page refreshes
-      userSubject.next(user.data)
-      localStorage.setItem("trclient", JSON.stringify(user.data))
-    })
-}
-
-function logout(): void {
-  // remove user from local storage, publish null to user subscribers, and redirect to login page
-  localStorage.removeItem("trclient")
-  userSubject.next(null)
-  Router.push("/login")
-}
-
-// function signup(data): Promise<void> {
-//   return fetchWrapper.post(`${baseUrl}/auth/signup/`, data);
+// function login(data): Promise<void> {
+//   return fetchWrapper
+//     .post(`${baseUrl}/auth/login/`, data)
+//     .then((user: typeof fetchWrapper.userValue) => {
+//       // publish user to subscribers and store in local storage to stay logged in between page refreshes
+//       userSubject.next(user.data)
+//       localStorage.setItem("trclient", JSON.stringify(user.data))
+//     })
 // }
 
-// function forgotPassword(data): Promise<void> {
-//   return fetchWrapper.post(`${baseUrl}/auth/forgot-password/`, data);
+// function logout(): void {
+//   // remove user from local storage, publish null to user subscribers, and redirect to login page
+//   localStorage.removeItem("trclient")
+//   userSubject.next(null)
+//   redirect("/login")
 // }
 
-// function resetPassword(data): Promise<void> {
-//   return fetchWrapper.post(`${baseUrl}/auth/reset-password/`, data);
-// }
+// // function signup(data): Promise<void> {
+// //   return fetchWrapper.post(`${baseUrl}/auth/signup/`, data);
+// // }
 
-function getUser(id: string): Promise<void> {
-  return fetchWrapper.get(`${baseUrl}/account_users/${id}/`)
-}
+// // function forgotPassword(data): Promise<void> {
+// //   return fetchWrapper.post(`${baseUrl}/auth/forgot-password/`, data);
+// // }
+
+// // function resetPassword(data): Promise<void> {
+// //   return fetchWrapper.post(`${baseUrl}/auth/reset-password/`, data);
+// // }
+
+// function getUser(id: string): Promise<void> {
+//   return fetchWrapper.get(`${baseUrl}/account_users/${id}/`)
+// }
--- src/shared/react-query/ReactQueryProvider.tsx
+++ src/shared/react-query/ReactQueryProvider.tsx
@@ -4,54 +4,42 @@ import { QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { queryClient } from "@/shared"
 import { userService } from "@/services"
-import { useRouter, usePathname, useSearchParams } from "next/navigation"
+import {
+  useRouter,
+  usePathname,
+  useSearchParams,
+  redirect,
+} from "next/navigation"
 
 export const ReactQueryProvider = ({
   children,
 }: {
   children: React.ReactNode
 }) => {
-  const router = useRouter()
   const pathname = usePathname()
-  const searchParams = useSearchParams()
-  const [authorized, setAuthorized] = useState(true)
-
-  console.log("ReactQueryProvider", { pathname, searchParams })
 
   // redirect to login page if accessing a private page and not logged in
 
-  useEffect(() => {
-    // run auth check on initial load
-    authCheck(pathname)
-  }, [])
+  // run auth check on initial load
 
-  function authCheck(url: string) {
-    // redirect to login page if accessing a private page and not logged in
-    const publicPaths = [
-      "/login",
-      "/forgot-password",
-      "/register",
-      "/",
-      "/password-reset",
-      "/verify",
-      "/verify/reset",
-    ]
-    const path = url
+  const publicPaths = [
+    "/login",
+    "/forgot-password",
+    // "/",
+    "/register",
+    "/password-reset",
+    "/verify",
+    "/verify/reset",
+  ]
 
-    if (!userService.userValue && !publicPaths.includes(path)) {
-      setAuthorized(false)
-      router.push("/login")
-    } else {
-      setAuthorized(true)
-    }
+  if (!userService.userValue && !publicPaths.includes(pathname)) {
+    redirect("/login")
   }
 
   return (
-    authorized && (
-      <QueryClientProvider client={queryClient}>
-        {children}
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    )
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
