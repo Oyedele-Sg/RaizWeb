e41c0ad7fff7153fb1c61c1689dc54e549d83cf2
Segun Oyedele <oyedelesegunfunmi@gmail.com>
2023-11-29 16:38:04 -0600

    testting google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import ReactGA from 'react-ga4';
+ReactGA.initialize(process.env.GA_TRACKING_ID as string);
 import { ReactQueryProvider } from '@/shared';
 import './globals.css';
 import React from 'react';
