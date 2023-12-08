78e3ea4f70344ceaea1d73dfeac760dc4aa4673c
Segun Oyedele <oyedelesegunfunmi@gmail.com>
2023-12-08 00:11:01 -0600

    Added google analytics tracking framework

---
+++ src/utils/google-analytics/events/signUpForm.tsx
@@ -0,0 +1,10 @@
+const UserSignUpFormCategory: string = 'User SignUp';
+const UserSignUpStartedAction: string = 'Started';
+const UserSignUpCompletedAction: string = 'Completed';
+
+export default UserSignUpFormCategory;
+export {
+  UserSignUpFormCategory,
+  UserSignUpStartedAction,
+  UserSignUpCompletedAction,
+};
