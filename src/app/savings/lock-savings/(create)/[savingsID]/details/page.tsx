a945bd3b7b648f38335e17a87302105145b72496
Segun Oyedele <oyedelesegunfunmi@gmail.com>
2024-01-17 20:36:11 -0600

    fixed utc future local date and savings navigation

--- src/app/savings/lock-savings/(create)/[savingsID]/details/page.tsx
+++ src/app/savings/lock-savings/(create)/[savingsID]/details/page.tsx
@@ -1,7 +1,7 @@
-"use client"
-import { HomeHeader } from "@/components/ajo"
-import { toast } from "@/components/ui/use-toast"
-import { userService } from "@/services"
+'use client';
+import { HomeHeader } from '@/components/ajo';
+import { toast } from '@/components/ui/use-toast';
+import { userService } from '@/services';
 import {
   AjoCycleInterface,
   AjoDataInterface,
@@ -12,183 +12,183 @@ import {
   TargetSavingsGroupDataInterface,
   LockSavingsDataInterface,
   WhiteTileWrap,
-} from "@/shared"
-import Image from "next/image"
-import { useRouter, useSearchParams, useParams } from "next/navigation"
-import React, { useEffect } from "react"
-import AjoPaymentTable from "@/components/ajo/AjoPaymentTable"
-import { useAppDispatch } from "@/shared/redux/types"
-import { setLoadingFalse, setLoadingTrue } from "@/shared/redux/features"
-import { dateDifferenceInDays, timeAgo } from "@/utils/helpers"
-import moment from "moment"
-import { SavingDetailsTile } from "@/components/savings"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Progress } from "@/components/ui/progress"
+} from '@/shared';
+import Image from 'next/image';
+import { useRouter, useSearchParams, useParams } from 'next/navigation';
+import React, { useEffect } from 'react';
+import AjoPaymentTable from '@/components/ajo/AjoPaymentTable';
+import { useAppDispatch } from '@/shared/redux/types';
+import { setLoadingFalse, setLoadingTrue } from '@/shared/redux/features';
+import { dateDifferenceInDays, getTimeAgoFromUTC } from '@/utils/helpers';
+import moment from 'moment';
+import { SavingDetailsTile } from '@/components/savings';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Progress } from '@/components/ui/progress';
 
 function Page() {
-  const Router = useRouter()
-  const dispatch = useAppDispatch()
-  const Params = useParams()
+  const Router = useRouter();
+  const dispatch = useAppDispatch();
+  const Params = useParams();
   const [savingsDetails, setSavingsDetails] =
-    React.useState<LockSavingsDataInterface>()
+    React.useState<LockSavingsDataInterface>();
   const [savingsPenalty, setSavingsPenalty] =
-    React.useState<EarlyPenaltyDataInterface>()
+    React.useState<EarlyPenaltyDataInterface>();
 
   const getData = async () => {
     try {
-      const response = await userService.getLockSavingsByID(Params.savingsID)
-      setSavingsDetails(response)
+      const response = await userService.getLockSavingsByID(Params.savingsID);
+      setSavingsDetails(response);
     } catch (error) {
       toast({
-        title: "Something Went Wrong",
+        title: 'Something Went Wrong',
         description: `${error}`,
-        variant: "destructive",
+        variant: 'destructive',
         style: {
-          backgroundColor: "#f44336",
-          color: "#fff",
-          top: "20px",
-          right: "20px",
+          backgroundColor: '#f44336',
+          color: '#fff',
+          top: '20px',
+          right: '20px',
         },
         duration: 5000,
-      })
+      });
     }
-  }
+  };
 
   const getPenaltyData = async () => {
     const data = {
       amount: savingsDetails?.lock_save_amount as number,
       end_date: savingsDetails?.end_date as Date,
       withdraw_date: new Date(),
-    }
+    };
     try {
-      const response = await userService.earlyWithdrawalPenalty(data)
+      const response = await userService.earlyWithdrawalPenalty(data);
 
-      setSavingsPenalty(response)
+      setSavingsPenalty(response);
     } catch (error) {
       toast({
-        title: "Something Went Wrong",
+        title: 'Something Went Wrong',
         description: `${error}`,
-        variant: "destructive",
+        variant: 'destructive',
         style: {
-          backgroundColor: "#f44336",
-          color: "#fff",
-          top: "20px",
-          right: "20px",
+          backgroundColor: '#f44336',
+          color: '#fff',
+          top: '20px',
+          right: '20px',
         },
         duration: 5000,
-      })
+      });
     }
-  }
+  };
 
   const tileData = [
     {
-      title: "Start Date",
-      data: moment(savingsDetails?.start_date).format("DD MMMM, YYYY"),
+      title: 'Start Date',
+      data: moment(savingsDetails?.start_date).format('DD MMMM, YYYY'),
     },
     {
-      title: "End Date",
-      data: moment(savingsDetails?.end_date).format("DD MMMM, YYYY"),
+      title: 'End Date',
+      data: moment(savingsDetails?.end_date).format('DD MMMM, YYYY'),
     },
     {
-      title: "Interest (₦)",
+      title: 'Interest (₦)',
       data: `₦${
         savingsDetails?.interest_amount ? savingsDetails?.interest_amount : 0
       }`,
     },
     {
-      title: "Locked Amount",
+      title: 'Locked Amount',
       data: `₦${savingsDetails?.lock_save_amount}`,
     },
     {
-      title: "Interest (%)",
+      title: 'Interest (%)',
       data: `${savingsDetails?.interest_rate.interest_rate}%`,
     },
     {
-      title: "Days Left",
+      title: 'Days Left',
       data: savingsDetails?.days_left,
     },
-  ]
+  ];
 
   useEffect(() => {
-    getData()
-  }, [Params.savingsID])
+    getData();
+  }, [Params.savingsID]);
   useEffect(() => {
     if (savingsDetails) {
-      getPenaltyData()
+      getPenaltyData();
     }
-  }, [savingsDetails])
+  }, [savingsDetails]);
   return (
-    <div className=' '>
-      <div className=' p-10'>
-        <HomeHeader title='Saving Hub' link='/savings/hub' />
+    <div className=" ">
+      <div className=" p-10">
+        <HomeHeader title="Saving Hub" link="/savings/hub" />
       </div>
 
-      <div className=' w-full min-h-[360px] relative   '>
-        <Image src={`/images/frame-643.png`} fill={true} alt='' />
-        <div className=' absolute bg-overlay-10 top-0 bottom-0 left-0 right-0  '></div>
+      <div className=" w-full min-h-[360px] relative   ">
+        <Image src={`/images/frame-643.png`} fill={true} alt="" />
+        <div className=" absolute bg-overlay-10 top-0 bottom-0 left-0 right-0  "></div>
 
-        <div className=' absolute top-0  bottom-0 left-0 right-0    p-10 flex  justify-between '>
-          <div className='flex gap-8 items-center w-full    '>
+        <div className=" absolute top-0  bottom-0 left-0 right-0    p-10 flex  justify-between ">
+          <div className="flex gap-8 items-center w-full    ">
             <div
-              className=' hidden border-[2px] border-neutral-80 rounded-full p-3 items-center justify-center lg:flex  '
-              onClick={() => Router.back()}
+              className=" hidden border-[2px] border-neutral-80 rounded-full p-3 items-center justify-center lg:flex  "
+              onClick={() => Router.push('/savings/hub')}
             >
               <Image
-                src={"/icons/ajo-arrow-left.svg"}
+                src={'/icons/ajo-arrow-left.svg'}
                 width={48}
                 height={48}
-                alt=''
+                alt=""
               />
             </div>
-            <div className=' flex w-full  justify-between max-w-[50%] '>
-              <div className=' text-grey flex flex-col gap-4  '>
-                <div className=' flex flex-col  gap-2   '>
-                  <h1 className='  font-headline__large font-semibold  text-ellipsis overflow-hidden max-w-full whitespace-nowrap '>
+            <div className=" flex w-full  justify-between max-w-[50%] ">
+              <div className=" text-grey flex flex-col gap-4  ">
+                <div className=" flex flex-col  gap-2   ">
+                  <h1 className="  font-headline__large font-semibold  text-ellipsis overflow-hidden max-w-full whitespace-nowrap ">
                     {savingsDetails?.lock_save_description}
                   </h1>
                 </div>
-                <div className='flex flex-col gap-10 '>
-                  <div className=' flex gap-8 flex-wrap '>
+                <div className="flex flex-col gap-10 ">
+                  <div className=" flex gap-8 flex-wrap ">
                     {/* <div className=' flex flex-col  items-center  gap-2   '>
                       <span className=' text-grey font-semibold  '>
                         {savingsDetails?.target_save_group_members.length}
                       </span>
                       <span className=' text-grey font-semi-mid '>Members</span>
                     </div> */}
-                    <div className=' flex flex-col items-center  gap-2   '>
-                      <span className=' text-grey font-semibold  '>
+                    <div className=" flex flex-col items-center  gap-2   ">
+                      <span className=" text-grey font-semibold  ">
                         ₦{savingsDetails?.lock_save_amount}
                       </span>
-                      <span className=' text-grey font-semi-mid '>
+                      <span className=" text-grey font-semi-mid ">
                         Locked amount
                       </span>
                     </div>
-                    <div className=' flex flex-col items-center  gap-2   '>
-                      <span className=' text-grey font-semibold  '>
+                    <div className=" flex flex-col items-center  gap-2   ">
+                      <span className=" text-grey font-semibold  ">
                         ₦{savingsDetails?.interest_amount || 0}
                       </span>
-                      <span className=' text-grey font-semi-mid '>
+                      <span className=" text-grey font-semi-mid ">
                         Interest Earned
                       </span>
                     </div>
-                    <div className=' flex flex-col items-center  gap-2   '>
-                      <span className=' text-grey font-semibold  '>Paid</span>
-                      <span className=' text-grey font-semi-mid '>
+                    <div className=" flex flex-col items-center  gap-2   ">
+                      <span className=" text-grey font-semibold  ">Paid</span>
+                      <span className=" text-grey font-semi-mid ">
                         {savingsDetails?.has_withdrawn
-                          ? "Completed"
-                          : "In-progress"}
+                          ? 'Completed'
+                          : 'In-progress'}
                       </span>
                     </div>
                   </div>
                 </div>
-                <div className=' flex items-center gap-8 '>
+                <div className=" flex items-center gap-8 ">
                   <Progress
                     value={Math.ceil(
                       savingsDetails?.completion_percentage as number
                     )}
-                    className=' bg-pesaraise-10 progress '
-                  />{" "}
-                  <p className=' text-neutral-40 text-t-14 font-medium   '>
+                    className=" bg-pesaraise-10 progress "
+                  />{' '}
+                  <p className=" text-neutral-40 text-t-14 font-medium   ">
                     {Math.ceil(savingsDetails?.completion_percentage as number)}
                     %
                   </p>
@@ -199,23 +199,23 @@ function Page() {
         </div>
       </div>
 
-      <div className='p-10 mt-[94px] flex gap-16'>
-        <WhiteTileWrap extraStyle=' p-12 flex-1 flex flex-col gap-8  '>
-          <div className=' flex flex-col gap-8  '>
-            <div className='flex flex-col gap-6 '>
-              <h2 className=' text-purple font-semibold text-center text-t-24 '>
-                {" "}
+      <div className="p-10 mt-[94px] flex gap-16">
+        <WhiteTileWrap extraStyle=" p-12 flex-1 flex flex-col gap-8  ">
+          <div className=" flex flex-col gap-8  ">
+            <div className="flex flex-col gap-6 ">
+              <h2 className=" text-purple font-semibold text-center text-t-24 ">
+                {' '}
                 Payout Rules
               </h2>
-              <p className=' text-purple text-center text-t-16  '>
+              <p className=" text-purple text-center text-t-16  ">
                 {` Keeps Funds for a Fixed Term, Breaking the Lock Incurs a ${savingsPenalty?.penalty_fee_rate}%
                   Penalty!`}
               </p>
             </div>
             {!savingsDetails?.has_withdrawn && (
               <BtnMain
-                btnText='Withdraw'
-                btnStyle=' w-full text-purple border-neutral-30 border '
+                btnText="Withdraw"
+                btnStyle=" w-full text-purple border-neutral-30 border "
                 onClick={() =>
                   Router.push(
                     `/savings/lock-savings/${Params.savingsID}/withdrawal`
@@ -225,7 +225,7 @@ function Page() {
             )}
           </div>
 
-          <div className=' grid  grid-cols-2  gap-8 flex-wrap  '>
+          <div className=" grid  grid-cols-2  gap-8 flex-wrap  ">
             {tileData.map((item, index) => (
               <SavingDetailsTile
                 data={item.data}
@@ -237,10 +237,10 @@ function Page() {
         </WhiteTileWrap>
       </div>
     </div>
-  )
+  );
 }
 
-export default Page
+export default Page;
 
 // could be needed later
 {
--- src/app/savings/my-targets/[savingsID]/details/page.tsx
+++ src/app/savings/my-targets/[savingsID]/details/page.tsx
@@ -1,7 +1,7 @@
-"use client"
-import { HomeHeader } from "@/components/ajo"
-import { toast } from "@/components/ui/use-toast"
-import { userService } from "@/services"
+'use client';
+import { HomeHeader } from '@/components/ajo';
+import { toast } from '@/components/ui/use-toast';
+import { userService } from '@/services';
 import {
   AjoCycleInterface,
   AjoDataInterface,
@@ -11,138 +11,138 @@ import {
   TargetSavingsGroupDataInterface,
   PersonalTargetSavingsDataInterface,
   WhiteTileWrap,
-} from "@/shared"
-import Image from "next/image"
-import { useRouter, useSearchParams, useParams } from "next/navigation"
-import React, { useEffect } from "react"
-import AjoPaymentTable from "@/components/ajo/AjoPaymentTable"
-import { useAppDispatch } from "@/shared/redux/types"
-import { setLoadingFalse, setLoadingTrue } from "@/shared/redux/features"
-import { dateDifferenceInDays, timeAgo } from "@/utils/helpers"
-import moment from "moment"
+} from '@/shared';
+import Image from 'next/image';
+import { useRouter, useSearchParams, useParams } from 'next/navigation';
+import React, { useEffect } from 'react';
+import AjoPaymentTable from '@/components/ajo/AjoPaymentTable';
+import { useAppDispatch } from '@/shared/redux/types';
+import { setLoadingFalse, setLoadingTrue } from '@/shared/redux/features';
+import { dateDifferenceInDays, getTimeAgoFromUTC } from '@/utils/helpers';
+import moment from 'moment';
 import {
   DetailInformationComponent,
   SavingDetailsTile,
-} from "@/components/savings"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Progress } from "@/components/ui/progress"
+} from '@/components/savings';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Progress } from '@/components/ui/progress';
 
 function Page() {
-  const Router = useRouter()
-  const dispatch = useAppDispatch()
-  const Params = useParams()
+  const Router = useRouter();
+  const dispatch = useAppDispatch();
+  const Params = useParams();
   const [savingsDetails, setSavingsDetails] =
-    React.useState<PersonalTargetSavingsDataInterface>()
+    React.useState<PersonalTargetSavingsDataInterface>();
   const [savingsActivity, setSavingsActivity] =
-    React.useState<PersonalTargetSavingsActivitiesDataInterface[]>()
+    React.useState<PersonalTargetSavingsActivitiesDataInterface[]>();
 
   const getData = async () => {
     try {
       const response = await userService.getPersonalTargetSavingsByID(
         Params.savingsID
-      )
-      setSavingsDetails(response)
+      );
+      setSavingsDetails(response);
       const activityResponse = await userService.getPersonalTargetActivity(
         Params.savingsID
-      )
+      );
 
-      setSavingsActivity(activityResponse)
+      setSavingsActivity(activityResponse);
     } catch (error) {
       toast({
-        title: "Something Went Wrong",
+        title: 'Something Went Wrong',
         description: `${error}`,
-        variant: "destructive",
+        variant: 'destructive',
         style: {
-          backgroundColor: "#f44336",
-          color: "#fff",
-          top: "20px",
-          right: "20px",
+          backgroundColor: '#f44336',
+          color: '#fff',
+          top: '20px',
+          right: '20px',
         },
         duration: 5000,
-      })
+      });
     }
-  }
+  };
 
   const tileData = [
     {
-      title: "Start Date",
+      title: 'Start Date',
       data: moment(savingsDetails?.target_save.start_date).format(
-        "DD MMMM, YYYY"
+        'DD MMMM, YYYY'
       ),
     },
     {
-      title: "End Date",
+      title: 'End Date',
       data: moment(savingsDetails?.target_save.end_date).format(
-        "DD MMMM, YYYY"
+        'DD MMMM, YYYY'
       ),
     },
     {
-      title: "Target Amount",
+      title: 'Target Amount',
       data: savingsDetails?.target_save.target_amount,
     },
     {
-      title: "Current Amount",
+      title: 'Current Amount',
       data: savingsDetails?.current_amount,
     },
 
     {
-      title: "Days Left",
+      title: 'Days Left',
       data: dateDifferenceInDays(
         savingsDetails?.target_save.start_date as Date,
         savingsDetails?.target_save.end_date as Date
       ),
     },
-  ]
+  ];
 
   const preJoinData = [
     {
-      title: "Target amount",
+      title: 'Target amount',
       value: savingsDetails?.target_save.target_amount,
     },
     {
-      title: "Current amount",
+      title: 'Current amount',
       value: savingsDetails?.current_amount,
     },
 
     {
-      title: "Days Left",
+      title: 'Days Left',
       value: dateDifferenceInDays(
         savingsDetails?.target_save.start_date as Date,
         savingsDetails?.target_save.end_date as Date
       ),
     },
-  ]
+  ];
 
   useEffect(() => {
-    getData()
-  }, [Params.savingsID])
+    getData();
+  }, [Params.savingsID]);
   return (
-    <div className=' '>
-      <div className=' p-10'>
-        <HomeHeader title='Saving Hub' link='/savings/hub' />
+    <div className=" ">
+      <div className=" p-10">
+        <HomeHeader title="Saving Hub" link="/savings/hub" />
       </div>
 
-      <div className=' w-full min-h-[360px] relative   '>
-        <Image src={`/images/frame-643.png`} fill={true} alt='' />
-        <div className=' absolute bg-overlay-10 top-0 bottom-0 left-0 right-0  '></div>
+      <div className=" w-full min-h-[360px] relative   ">
+        <Image src={`/images/frame-643.png`} fill={true} alt="" />
+        <div className=" absolute bg-overlay-10 top-0 bottom-0 left-0 right-0  "></div>
 
-        <div className=' absolute top-0  bottom-0 left-0 right-0    p-10 flex  justify-between '>
-          <div className='flex gap-8 items-center w-full     '>
+        <div className=" absolute top-0  bottom-0 left-0 right-0    p-10 flex  justify-between ">
+          <div className="flex gap-8 items-center w-full     ">
             <div
-              className=' hidden border-[2px] border-neutral-80 rounded-full p-3 items-center justify-center lg:flex  '
-              onClick={() => Router.push("/savings/hub")}
+              className=" hidden border-[2px] border-neutral-80 rounded-full p-3 items-center justify-center lg:flex  "
+              onClick={() => Router.push('/savings/hub')}
             >
               <Image
-                src={"/icons/ajo-arrow-left.svg"}
+                src={'/icons/ajo-arrow-left.svg'}
                 width={48}
                 height={48}
-                alt=''
+                alt=""
               />
             </div>
-            <div className=' flex w-full  justify-between  '>
-              <div className=' text-grey flex flex-col gap-4  '>
-                <div className=' flex flex-col  gap-2  '>
-                  <h1 className='  font-headline__large font-semibold  '>
+            <div className=" flex w-full  justify-between  ">
+              <div className=" text-grey flex flex-col gap-4  ">
+                <div className=" flex flex-col  gap-2  ">
+                  <h1 className="  font-headline__large font-semibold  ">
                     {savingsDetails?.target_save.target_save_name}
                   </h1>
                 </div>
@@ -154,41 +154,41 @@ function Page() {
             </div>
           </div>
           <div
-            className='flex items-center gap-1 align-bottom self-end cursor-default '
+            className="flex items-center gap-1 align-bottom self-end cursor-default "
             onClick={() =>
               Router.push(`/savings/my-targets/${Params.savingsID}/settings`)
             }
           >
             <Image
-              src={"/icons/vuesax-outline-edit.svg"}
+              src={'/icons/vuesax-outline-edit.svg'}
               width={24}
               height={24}
-              alt=''
+              alt=""
             />
-            <span className=' text-neutral-20 text-t-16   '>Edit</span>
+            <span className=" text-neutral-20 text-t-16   ">Edit</span>
           </div>
         </div>
       </div>
 
-      <div className='p-10 mt-[94px] flex gap-16'>
-        <WhiteTileWrap extraStyle=' p-12 flex-1 flex flex-col gap-8  '>
-          <div className=' flex flex-col gap-8  '>
-            <div className='flex flex-col gap-6 '>
-              <h2 className=' text-purple font-semibold text-center text-t-24 '>
-                {" "}
+      <div className="p-10 mt-[94px] flex gap-16">
+        <WhiteTileWrap extraStyle=" p-12 flex-1 flex flex-col gap-8  ">
+          <div className=" flex flex-col gap-8  ">
+            <div className="flex flex-col gap-6 ">
+              <h2 className=" text-purple font-semibold text-center text-t-24 ">
+                {' '}
                 Payout Rules
               </h2>
-              <p className=' text-purple text-center text-t-16  '>
+              <p className=" text-purple text-center text-t-16  ">
                 Money to be collected on the first of December and no one has
                 access to your deposit
               </p>
             </div>
           </div>
 
-          <div className=' flex  gap-9  '>
+          <div className=" flex  gap-9  ">
             <BtnMain
-              btnText='Withdraw'
-              btnStyle=' w-full text-purple border-neutral-30 border '
+              btnText="Withdraw"
+              btnStyle=" w-full text-purple border-neutral-30 border "
               onClick={() =>
                 Router.push(
                   `/savings/my-targets/${Params.savingsID}/withdrawal`
@@ -196,15 +196,15 @@ function Page() {
               }
             />
             <BtnMain
-              btnText='Add Funds'
-              btnStyle=' btn-gradient-savings w-full btn text-grey'
+              btnText="Add Funds"
+              btnStyle=" btn-gradient-savings w-full btn text-grey"
               onClick={() =>
                 Router.push(`/savings/my-targets/${Params.savingsID}/add-funds`)
               }
             />
           </div>
 
-          <div className=' grid  grid-cols-2  gap-8 flex-wrap  '>
+          <div className=" grid  grid-cols-2  gap-8 flex-wrap  ">
             {tileData.map((item, index) => (
               <SavingDetailsTile
                 data={item.data}
@@ -214,38 +214,38 @@ function Page() {
             ))}
           </div>
         </WhiteTileWrap>
-        <WhiteTileWrap extraStyle=' p-12 flex-1 flex flex-col gap-8 '>
-          <div className=' flex flex-col gap-8  '>
-            <div className='flex flex-col gap-6 '>
-              <h2 className=' text-purple font-semibold text-t-24 '>
-                {" "}
+        <WhiteTileWrap extraStyle=" p-12 flex-1 flex flex-col gap-8 ">
+          <div className=" flex flex-col gap-8  ">
+            <div className="flex flex-col gap-6 ">
+              <h2 className=" text-purple font-semibold text-t-24 ">
+                {' '}
                 Activites
               </h2>
             </div>
           </div>
 
-          <div className=' flex gap-x-16 gap-y-8 flex-wrap  '>
+          <div className=" flex gap-x-16 gap-y-8 flex-wrap  ">
             {savingsActivity?.map((item, index) => (
-              <div className=' flex items-center gap-2   '>
-                <Avatar className=' cursor-default border-neutral-30 border-[2px] w-[48px] h-[48px]  '>
+              <div className=" flex items-center gap-2   ">
+                <Avatar className=" cursor-default border-neutral-30 border-[2px] w-[48px] h-[48px]  ">
                   <AvatarImage src={item.account_user.profile_image_url} />
-                  <AvatarFallback className=' text-purple font-bold  uppercase '>
+                  <AvatarFallback className=" text-purple font-bold  uppercase ">
                     {item.account_user.first_name.charAt(0)}
                     {item.account_user.last_name.charAt(0)}
                   </AvatarFallback>
                 </Avatar>
 
-                <div className=' flex flex-col gap-2 '>
-                  <h4 className=' text-purple text-t-18 font-semi-mid   '>
+                <div className=" flex flex-col gap-2 ">
+                  <h4 className=" text-purple text-t-18 font-semi-mid   ">
                     {item.activity_description}
                   </h4>
-                  <div className='  flex items-center gap-2 '>
-                    <div className=' text-neutral-70  text-t-16 '>
-                      {savingsDetails?.target_save.target_save_name}{" "}
-                    </div>{" "}
-                    <div className=' bg-neutral-70 rounded-full w-2 h-2    '></div>
-                    <div className=' text-neutral-70 text-t-16 '>
-                      {timeAgo(item.updated_at)}
+                  <div className="  flex items-center gap-2 ">
+                    <div className=" text-neutral-70  text-t-16 ">
+                      {savingsDetails?.target_save.target_save_name}{' '}
+                    </div>{' '}
+                    <div className=" bg-neutral-70 rounded-full w-2 h-2    "></div>
+                    <div className=" text-neutral-70 text-t-16 ">
+                      {getTimeAgoFromUTC(item.updated_at)}
                     </div>
                   </div>
                 </div>
@@ -255,7 +255,7 @@ function Page() {
         </WhiteTileWrap>
       </div>
     </div>
-  )
+  );
 }
 
-export default Page
+export default Page;
